e307794954a04dd055aa70dd79e1482cdba5957b
The Joker <54842770+Nbruunj@users.noreply.github.com>
2021-03-24 10:26:27 +0100

    Initial commit

---
+++ src/app/chat/chat.component.ts
@@ -0,0 +1,87 @@
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {ChatService} from './shared/chat.service';
+import {Observable, Subject, Subscription} from 'rxjs';
+import {debounceTime, map, take, takeUntil} from 'rxjs/operators';
+import {ChatClient} from './shared/chat-client.model';
+import {ChatMessage} from './shared/chat-message.model';
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styleUrls: ['./chat.component.css']
+})
+export class ChatComponent implements OnInit, OnDestroy {
+  messageFc = new FormControl('');
+  nameFC = new FormControl('');
+  messages: ChatMessage[] = [];
+  clientsTyping: ChatClient[] = [];
+  unsubscribe$ = new Subject();
+  clients$: Observable<ChatClient[]> | undefined;
+  chatClient: ChatClient | undefined;
+  error$: Observable<string> | undefined;
+  constructor(private chatService: ChatService) { }
+
+  ngOnInit(): void {
+    this.messageFc.valueChanges
+      .pipe(
+        takeUntil(this.unsubscribe$),
+        debounceTime(500)
+      )
+      .subscribe((value) => this.chatService.sendTyping(value.length > 0));
+
+    this.clients$ = this.chatService.listenForClients();
+
+    this.chatService.listenForMessages()
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(message => {
+        this.messages.push(message)
+      });
+
+
+    this.chatService.listenForClientTyping()
+      .pipe(takeUntil(this.unsubscribe$)
+      )
+      .subscribe((chatClient) => {
+        if(chatClient.typing && !this.clientsTyping.find((c) => c.id === chatClient.id)) {
+          this.clientsTyping.push(chatClient);
+        } else {
+          this.clientsTyping = this.clientsTyping.filter((c) => c.id !== chatClient.id);
+        }
+      })
+
+    this.chatService.listenForWelcome()
+      .pipe(
+        takeUntil(this.unsubscribe$)
+      )
+      .subscribe(welcome => {
+        this.messages = welcome.messages;
+        this.chatClient = this.chatService.chatClient = welcome.client;
+      })
+
+    if(this.chatService.chatClient) {
+      this.chatService.sendName(this.chatService.chatClient.name);
+    }
+
+    this.error$ = this.chatService.listenForError().pipe(takeUntil(this.unsubscribe$));
+  }
+
+  sendMessage(): void {
+    console.log(this.messageFc.value);
+    this.chatService.sendMessage(this.messageFc.value);
+    this.messageFc.patchValue('');
+  }
+
+  ngOnDestroy(): void {
+    console.log('Destroyed');
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
+  sendName(): void {
+    if(this.nameFC.value)
+    {
+      this.chatService.sendName(this.nameFC.value);
+    }
+  }
+}
